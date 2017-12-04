193f0bcc95683c09be97896944836444fb9bf20a
jengleberg <jeffengleberg@hotmail.com>
2017-12-04 16:09:43 -0700

    updates config/app.php with error log for heroku

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,15 +13,15 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->integer('is_admin')->unsigned()->default(0);
-            $table->rememberToken();
-            $table->timestamps();
-        });
+        // Schema::create('users', function (Blueprint $table) {
+        //     $table->increments('id');
+        //     $table->string('name');
+        //     $table->string('email')->unique();
+        //     $table->string('password');
+        //     $table->integer('is_admin')->unsigned()->default(0);
+        //     $table->rememberToken();
+        //     $table->timestamps();
+        // });
     }
 
     /**
