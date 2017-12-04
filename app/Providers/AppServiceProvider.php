564a40c46eb8073d5c13c1d163c46424cbc45637
jengleberg <jeffengleberg@hotmail.com>
2017-12-04 15:09:07 -0700

    updates databaseconfig for heroku

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,6 +13,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        Schema::defaultStringLength(191);
+        
         view()->composer('dashboard', function ($view) {
 
             $archives = \App\Task::archives();
